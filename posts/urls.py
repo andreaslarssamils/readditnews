497837b95cce953260581b3133da99f56199bbce
andreaslarssamils <larssamils.andreas@gmail.com>
2026-04-14 05:20:50 +0200

    Add posts URLs and templates

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("post/create/", views.PostCreateView.as_view(), name="post_create"),
+]
--- readditnews/urls.py
+++ readditnews/urls.py
@@ -23,4 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("", include("posts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
